584ab604d06d54c5170f6e98fd7e86010a2d986a
agdcarlosa <agdcarlosa@hotmail.com>
2015-07-10 10:22:30 +0200

    Busquedas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -12,10 +12,11 @@ exports.load = function(req, res, next, quizId) {
    ).catch(function(error) { next(error);}); 
 }; 
  
-
 // GET /quizes 
-exports.index = function(req, res) { 
-   models.Quiz.findAll().then( 
+exports.index = function(req, res) {
+	 var w="";
+	 if(req.query.search !=="") w = {where: ["pregunta like ?", '%'+req.query.search.replace(/ /g,'%')+'%']};
+   models.Quiz.findAll(w).then( 
      function(quizes) { 
        res.render('quizes/index', { quizes: quizes}); 
      } 
