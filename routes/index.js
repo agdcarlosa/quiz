9e339c42d4d75d93862d793519d0a95e4b2e79fd
agdcarlosa <agdcarlosa@hotmail.com>
2015-08-05 18:24:35 +0200

    Moderación de Comentarios

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,8 @@ router.get('/author', function(req, res) {
 
 //Autoload de comandos con :quizId
 //
-router.param('quizId', quizController.load);// autoload :quizId})d
+router.param('quizId', quizController.load);// autoload :quizId
+router.param('commentId', commentController.load);// autoload :commentId
 
 //Definición de rutas de sesión
 router.get('/login',  sessionController.new);			//formulario login
@@ -37,7 +38,9 @@ router.get('/quizes/:quizId(\\d+)/edit',   sessionController.loginRequired,quizC
 router.put('/quizes/:quizId(\\d+)',        sessionController.loginRequired,quizController.update);
 router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired,quizController.destroy);
 
+//Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new',commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',   commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',sessionController.loginRequired,commentController.publish);
 
 module.exports = router;
